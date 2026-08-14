43212c68c36cd7966f5ce1aeec15d0bc7c5de07f
Mariana Francisca Parra Soto <43527234+MariParra@users.noreply.github.com>
2026-08-14 14:41:27 -0400

    fix scroll

--- app/2_scripts/catalogo.py
+++ app/2_scripts/catalogo.py
@@ -32,9 +32,13 @@ except KeyError:
 
 html_scroll_indicator = """
     <div class="scroll-indicator" onclick="
-        const rootElement = window.parent.document.scrollingElement || window.parent.document.body;
-        rootElement.scrollBy({ top: 600, behavior: 'smooth' });
-    ;" title="Bajar">
+        const container = window.parent.document.querySelector('.stApp') || window.parent.document.scrollingElement;
+        if (container) {
+            container.scrollBy({ top: 600, behavior: 'smooth' });
+        } else {
+            window.parent.scrollBy({ top: 600, behavior: 'smooth' });
+        }
+    " title="Bajar">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path d="M0 0h24v24H0V0z" fill="none"/>
             <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/>
